2363da1b4464abb6c657200e96688071dcf38ed1
youngsik_won <youngsik3200@gmail.com>
2020-02-19 01:49:00 +0900

    Initial commit

---
+++ src/test/java/con/cos/insta/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package con.cos.insta.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
