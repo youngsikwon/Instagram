ca992eee799df70dbd04420f52630ace637328f6
youngsik_won <youngsik3200@gmail.com>
2020-02-20 02:51:46 +0900

    Start your Instagram Proejct

--- src/main/java/con/cos/insta/DemoApplication.java
+++ src/main/java/con/cos/insta/DemoApplication.java
@@ -1,4 +1,4 @@
-package con.cos.insta.demo;
+package con.cos.insta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DemoApplication.class, args);
     }
 
--- src/test/java/con/cos/insta/DemoApplicationTests.java
+++ src/test/java/con/cos/insta/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package con.cos.insta.demo;
+package con.cos.insta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
