5017034cf3aa1183bcdaa69ef85a9438f356420b
youngsik_won <youngsik3200@gmail.com>
2020-05-20 22:37:00 +0900

    Follow fetch api를 사용하여 팔로우 구현 예정.

--- src/main/java/com/cos/insta/test/TestController.java
+++ src/main/java/com/cos/insta/test/TestController.java
@@ -3,18 +3,34 @@ package com.cos.insta.test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.cos.insta.model.Image;
 import com.cos.insta.model.Likes;
 import com.cos.insta.model.User;
 
+import com.cos.insta.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class TestController {
+
+
+    @Autowired
+    private UserRepository mUserRepository;
+
+    @GetMapping("/test/user/{id}")
+    public @ResponseBody User testUser(@PathVariable int id){
+     Optional<User>  oUser = mUserRepository.findById(id);
+     User user = oUser.get();
+
+     return user;
+    }
 
     @GetMapping("/test/home")
     public String testHome() {
@@ -160,4 +176,6 @@ public class TestController {
     public String testExplore() {
         return "image/explore";
     }
+
+
 }
